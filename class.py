33ed7792dc09f756894b5e6f684ddb66dc9263f1
Shelton-debug <sheltpnotieno47@gmail.com>
2025-10-21 00:43:09 +0300

    matplotlibpie

--- class.py
+++ class.py
@@ -1,13 +1,9 @@
-import turtle
+from matplotlib import pyplot as plt 
 
-t = turtle.Turtle()
-colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
-turtle.bgcolor('black')
-
-for x in range(360):
-    t.pencolor(colors[x % 6])
-    t.width(x / 100 + 1)
-    t.forward(x)
-    t.left(59)
-    
-turtle.done()
+labels = ['python', 'java', 'c++', 'ruby', 'javascript']
+sizes = [215, 130, 245, 210, 210]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'lightgreen']
+explode = (0.1, 0, 0, 0, 0)
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
